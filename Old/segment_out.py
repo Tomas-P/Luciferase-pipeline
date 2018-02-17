59c4d9161405c2553e6766fecfd045ebd80b1282
Tomas-P <32210966+Tomas-P@users.noreply.github.com>
2018-02-16 20:19:44 -0500

    no longer needed

--- Old/segment_out.py
+++
@@ -1,9 +0,0 @@
-def search(IJout, _split="[DEBUG]", tag="loc = ["):
-    sections = IJout.split(_split)
-    for section in sections:
-        if tag in section:
-            return section
-
-if __name__=='__main__':
-    imagej_out = input()
-    locations = search(imagej_out)

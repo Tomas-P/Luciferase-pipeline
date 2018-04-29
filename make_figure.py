f331ad4516a8248945b4b066d7f1b81ba903f6bc
Tomas-P <32210966+Tomas-P@users.noreply.github.com>
2018-04-29 17:05:33 -0400

    Add ability to make line graphs
    Add ability to make line graphs based on the data in the analysis.

---
+++ make_figure.py
@@ -0,0 +1,24 @@
+import align_temporal
+from matplotlib import pyplot
+
+def grids():
+    'Get the grids using the align_temporal lib'
+    name = align_temporal.get_csv_name()
+    csv = align_temporal.read_csv(name)
+    two_dim = align_temporal.to_two_d(csv)
+    means,medians = align_temporal.to_grids(two_dim)
+    return means,medians
+
+def example_1():
+    'Display the means of each plant over time.'
+    means,medians = grids()
+    # My idea of time and pos is transposed from pyplot's idea of time and pos
+    pyplot.plot(means.T)
+    pyplot.show()
+
+def example_2():
+    'Display the medians of each plant over time.'
+    means,medians = grids()
+    # My idea of time and place is transposed from pyplot's idea of time and place
+    pyplot.plot(medians.T)
+    pyplot.show()

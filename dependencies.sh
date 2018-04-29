06c892b7b41d2c7d23eb5fd9afd1311f90039246
Tomas-P <32210966+Tomas-P@users.noreply.github.com>
2018-04-29 17:29:59 -0400

    Add dependency installer scripts

---
+++ dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# this file is for installing the dependencies on linux
+# it does the same thing as the one for windows
+pip install numpy
+pip install pandas
+pip install matplotlib
+#numpy is a dependency of pandas and therefore must be installed first
+# This is the same installation order I used, albiet on a windows computer.

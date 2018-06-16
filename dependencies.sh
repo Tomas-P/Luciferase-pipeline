a43e73c2c149acacc9152027eb49fd1d3df05af5
Tomas-P <32210966+Tomas-P@users.noreply.github.com>
2018-06-16 15:22:02 -0400

    Delete dependencies.sh

--- dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# this file is for installing the dependencies on linux
-# it does the same thing as the one for windows
-pip install numpy
-pip install pandas
-pip install matplotlib
-#numpy is a dependency of pandas and therefore must be installed first
-# This is the same installation order I used, albiet on a windows computer.

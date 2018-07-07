3d1be934890b87419630c4f511a1421af3258030
Tomas-P <32210966+Tomas-P@users.noreply.github.com>
2018-07-07 19:30:28 -0400

    Reorganize files

---
+++ Process/Tomas_Plugin.py
@@ -0,0 +1,100 @@
+from ij import IJ
+from ij.plugin.frame import RoiManager
+from ij.plugin import ImageCalculator
+from xml.etree import ElementTree as et
+import glob
+import sys
+
+def make_mask(image):
+	IJ.run(image, "Robust Automatic Threshold Selection", 
+	"noise=25 lambda=3 min=410")
+	mask = IJ.getImage()
+	IJ.run(mask, "16-bit", "")
+	IJ.run(mask, "Multiply...", "value=257") # 255 * 257 == 65535 == 2**16-1
+	IJ.run(mask, "Median...", "radius=2") # remove some of the noise
+	return mask
+
+
+def open_sequence(xml_tree):
+	foldername = xml_tree.findtext("images")
+	filename = sorted(glob.glob(foldername+'*'))[0]
+	IJ.run("Image Sequence...","open={} sort".format(filename))
+	stack = IJ.getImage()
+	IJ.run(stack, "Enhance Contrast...", 
+	"saturated=0.3 equalize process_all")
+	IJ.run(stack, "Median...", "radius=2 stack")
+	IJ.run(stack, "Subtract Background...", "rolling=50 stack")
+	return stack
+
+def SIFT_register(stack):
+	IJ.run(stack, "Linear Stack Alignment with SIFT", "initial_gaussian_blur=1.60 steps_per_scale_octave=3 minimum_image_size=64 maximum_image_size=1024 feature_descriptor_size=4 feature_descriptor_orientation_bins=8 closest/next_closest_ratio=0.92 maximal_alignment_error=25 inlier_ratio=0.05 expected_transformation=Rigid interpolate")
+	return IJ.getImage()
+
+def xmltree():
+	script_file = sys.argv[0]
+	folder = script_file[:script_file.rfind('/')]+'/'
+	xfile = folder + 'info.xml'
+	return et.parse(xfile)
+
+def apply_mask(im_calc,stack, mask):
+	image = im_calc.run("AND create stack", stack, mask)
+	return image
+
+def set_measurements():
+	IJ.run("Set Measurements...",
+	"area mean area_fraction stack redirect=None decimal=3")
+
+def measure(roi_manager, stack):
+	for i in range(stack.getImageStackSize()):
+		roi_manager.runCommand(stack,"Measure")
+		IJ.run(stack, "Next Slice [>]", "")
+
+def save_results(filename):
+	IJ.saveAs("Results", filename)
+
+def open_rois(tree):
+	roi_set = tree.findtext("ROI_archive")
+	rm = RoiManager()
+	rm.runCommand("Open", roi_set)
+	return rm
+
+def get_background_comparison(stack, tree):
+	# The background mean is the only important thing
+	IJ.run("Set Measurements...", "mean stack redirect=None decimal=3")
+	# read in the selection info from the xml tree
+	bx,by,width, height = eval(tree.findtext("bg"))
+	# create a rectangular selection
+	stack.setRoi(bx,by,width,height)
+	# measure the selection in each slice
+	for i in range(stack.getImageStackSize()):
+		IJ.run(stack, "Measure", "")
+		IJ.run(stack, "Next Slice [>]", "")
+	# save the results
+	IJ.saveAs("Results", tree.findtext("bgcsv"))
+	# clean up for the actual program
+	IJ.run("Clear Results", "")
+	
+def main():
+	xtree = xmltree()
+	calc = ImageCalculator()
+	proto_stack = open_sequence(xtree)
+	stack = SIFT_register(proto_stack)
+	get_background_comparison(stack, xtree)
+	mask = make_mask(stack)
+	mask.show()
+	filtered = apply_mask(calc,stack, mask)
+	filtered.show()
+	set_measurements()
+	roi_manager = open_rois(xtree)
+	measure(roi_manager, filtered)
+	save_results(xtree.findtext("csv"))
+	roi_manager.close()
+
+main()
+
+IJ.run("Close All", "")
+IJ.selectWindow("Results")
+IJ.run("Close")
+IJ.selectWindow("Log")
+IJ.run("Close")
+IJ.run("Quit")
---
+++ Segment/Setaria.py
@@ -0,0 +1,144 @@
+#@File (label="Image") imagefile
+#@int roi_width
+#@int roi_height
+#@File (label="Save ROI Archive as") archive
+
+from ij import IJ
+from ij.gui import PolygonRoi
+from ij.gui import Roi
+from ij.plugin.frame import RoiManager
+from math import sqrt
+
+def getinputs():
+	return (str(imagefile), int(roi_width), int(roi_height))
+
+def getmask(filename):
+	image = IJ.openImage(filename)
+	image.show()
+	IJ.run(image, "Enhance Contrast...", "saturated=0.3 equalize")
+	IJ.run(image, "Subtract Background...", "rolling=50")
+	IJ.run(image, "Median...", "radius=2")
+	IJ.run(image, "Robust Automatic Threshold Selection", "noise=25 lambda=3 min=410")
+	mask = IJ.getImage()
+	image.close()
+	return mask
+
+def distance(pixel1, pixel2):
+	xpart = (pixel1[0] - pixel2[0])**2
+	ypart = (pixel1[1] - pixel2[1])**2
+	return sqrt(xpart + ypart)
+
+def get_region_pixels(image, width, height, startx, starty):
+	pixels = []
+	for x in range(startx, startx + width):
+		for y in range(starty, starty + height):
+			if image.getPixel(x,y)[0]:
+				pixels.append((x,y,image.getPixel(x,y)[0]))
+
+	return pixels
+
+def get_border_pixels(image, pixels):
+	border_pixels = []
+	getpixel = lambda x,y : image.getPixel(x,y)[0]
+
+	for pixel in pixels:
+		x = pixel[0]
+		y = pixel[1]
+		ncount = 0
+
+		if getpixel(x + 1, y):
+			ncount += 1
+		if getpixel(x - 1, y):
+			ncount += 1
+		if getpixel(x, y + 1):
+			ncount += 1
+		if getpixel(x, y - 1):
+			ncount += 1
+		if getpixel(x + 1, y + 1):
+			ncount += 1
+		if getpixel(x - 1, y - 1):
+			ncount += 1
+		if getpixel(x - 1, y + 1):
+			ncount += 1
+		if getpixel(x + 1, y - 1):
+			ncount += 1
+
+		if ncount < 8:
+			border_pixels.append(pixel)
+
+	return border_pixels
+
+def sort_pixels(pixels):
+	sorted_pixels = [pixels[0]]
+
+	while pixels:
+
+		dist = 1000
+		prev = sorted_pixels[-1]
+		next = None
+	
+		for p in pixels:
+			if distance(p, prev) < dist:
+				next = p
+				dist = distance(p, prev)
+
+		sorted_pixels.append(next)
+		pixels.remove(next)
+
+	return sorted_pixels
+
+def make_roi(sorted_pixels):
+	xlist = []
+	ylist = []
+	pcount = len(sorted_pixels)
+	for x,y,val in sorted_pixels:
+		xlist.append(x)
+		ylist.append(y)
+
+	roi = PolygonRoi(xlist, ylist, pcount, PolygonRoi.POLYGON)
+	return roi
+
+def get_manager_with_rois():
+	filename, rwidth, rheight = getinputs()
+	mask = getmask(filename)
+	mask.show()
+	rm = RoiManager()
+	area = rwidth * rheight
+	
+	for y in range(0,mask.getHeight(),rheight):
+		for x in range(0,mask.getWidth(),rwidth):
+			region_pixels = get_region_pixels(mask,rwidth,rheight,x,y)
+			if not region_pixels:
+				continue # skip empty ones
+			border_pixels = get_border_pixels(mask,region_pixels)
+			if not border_pixels:
+				# we know this region is not empty
+				# we also know this region has no 
+				# boundaries.
+				# Therefore, all of this region is an Roi
+				
+				# add the area to the roi manager
+				r = Roi(x,y,rwidth,rheight)
+				mask.setRoi(r)
+				if r.getStatistics().areaFraction == 100:
+					rm.addRoi(r)
+					
+				# proceed normally with the rest of the image
+				continue
+			sorted_pixels = sort_pixels(border_pixels)
+			roi = make_roi(sorted_pixels)
+			mask.setRoi(roi)
+
+			stats = roi.getStatistics()
+			if stats.areaFraction > 90 and stats.area > 1.0/16.0 * area:
+				rm.addRoi(roi)
+	return rm, mask
+
+def main():
+	manager, imageplus = get_manager_with_rois()
+	manager.runCommand("Save", str(archive))
+	manager.runCommand(imageplus, "Show All with labels")
+
+main()
+
+#IJ.run("Quit") # only needed if made part of pipeline

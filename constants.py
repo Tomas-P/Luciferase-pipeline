26249b3728c416b3f8cb15d312ed1e8ed447c970
Tomas-P <32210966+Tomas-P@users.noreply.github.com>
2018-07-12 13:23:27 -0400

    Add useful constants

---
+++ constants.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 10 10:07:43 2018
+
+@author: tomas
+"""
+from os import path as _path
+class CONSTANTS:
+    
+    ARABADOPSIS = 0
+    SETARIA = 1
+    OTHER = 2
+    IMAGEJ = _path.expanduser("~/Fiji.app/ImageJ-linux64")
+    
+    def __setattr__(self,name,value):
+        raise TypeError("Constants cannot be altered!")
+    
+
+CONSTANTS = CONSTANTS()

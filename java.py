fb8eefdec0f80a6b2a0fde64e4910aff9156af9f
Tomas-P <32210966+Tomas-P@users.noreply.github.com>
2020-12-26 15:08:17 -0500

    like before, but again

--- java.py
+++ java.py
@@ -9,15 +9,15 @@ def prep_env():
     system = platform.system()
 
     if system == "Linux":
-        path = Path(".") / "javadevkit" / "linux" / "jdk-15.0.1"
+        path = Path(".") / "jdk-15.0.1"
     elif system == "Windows":
-        path = Path(".") / 'javadevkit' / 'windows' / 'jdk-15.0.1'
+        path = Path(".") / 'jdk-15.0.1'
         os.environ['PATH'] += str(path / 'bin') + ';'
         os.environ['PATH'] += str(path / 'bin' / 'server') + ';'
         os.environ['PATH'] += str(path / 'lib') + ';'
     elif system == 'Darwin':
         raise Warning("This program has not been tested in MacOS")
-        path = Path('.') / 'javadevkit' / 'osx' / 'jdk-15.0.1.jdk'
+        path = Path('.') / 'jdk-15.0.1.jdk'
         path = path / 'Contents' / 'Home'
     else:
         raise Exception(f"{system} is not supported by this program")
@@ -25,15 +25,6 @@ def prep_env():
     os.environ['JAVA_HOME'] = str(path.absolute())
 
 def locate_jars():
-    system = platform.system()
-    if system == "Linux":
-        subdir = "linux"
-    elif system == "Windows":
-        subdir = "windows"
-    elif system == "Darwin":
-        subdir = "mac"
-    else:
-        raise Exception(f"{system} is not supported by this program")
     ijfolder = Path('.').absolute() / "Fiji.app"
     return [str(p) for p in ijfolder.glob("**/*.jar")]
 

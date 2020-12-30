b6cbf7e07443f41536029b45849c99275900a6ce
Tomas-P <32210966+Tomas-P@users.noreply.github.com>
2020-12-30 17:30:34 -0500

    Make x-axis measure elapsed time

--- graph.py
+++ graph.py
@@ -1,11 +1,11 @@
 from matplotlib import pyplot
 from pathlib import Path
 from parameter import Param
+import numpy
 
 def labels(arguments :dict):
     "set standard labels for axes"
-    xlabel = f"""First image taken at {arguments[Param.INIT]} with \
-elapsed time between images being {arguments[Param.ELAPSED]} hours"""
+    xlabel = f"Elapsed time since {arguments[Param.INIT]} in hours"
     pyplot.xlabel(xlabel)
     ylabel = "{}relative intensity".format(
                 "normalized " if arguments[Param.NORM] else ""
@@ -22,14 +22,17 @@ def summary(arguments :dict, affiliation :dict, background :list, groups :list):
     pyplot.title("Summary")
     for i,g in enumerate(groups):
         line = aggregate_group_into_line(affiliation, g)
-        pyplot.plot(line, label=f"{i}")
+        xticks = float(arguments[Param.ELAPSED]) * numpy.arange(line.size)
+        pyplot.plot(xticks, line, label=f"Group {i}")
     pyplot.plot(background, label="background")
     pyplot.legend()
 
 def grapheachgroup(arguments:dict, affiliation:dict, background:list, groups:list):
     "graph each group in a separate graph"
     for i,g in enumerate(groups):
-        pyplot.plot(affiliation[g])
+        points = len(affiliation[g].mean(axis=1))
+        xticks = float(arguments[Param.ELAPSED]) * numpy.arange(points)
+        pyplot.plot(xticks, affiliation[g])
         pyplot.title(f"Group {i}")
         labels(arguments)
         yield i
@@ -40,7 +43,9 @@ def graphalldata(arguments, affiliation, background, groups):
     labels(arguments)
     pyplot.title("All plants shown")
     for g in groups:
-        pyplot.plot(affiliation[g])
+        points = len(affiliation[g].mean(axis=1))
+        xticks = float(arguments[Param.ELAPSED]) * numpy.arange(points)
+        pyplot.plot(xticks, affiliation[g])
 
 def savefig(arguments, filename):
     "save a figure then clear the buffer"
